6c7a1764fdf5ae057bfba93fb19a0d5182762756
mufiii <mufeesrahman9795@gmail.com>
2025-12-29 12:19:32 +0530

    Fix Amplify static export (index.html)

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',          // ← THIS IS THE FIX
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
- 
 }
 
 export default nextConfig
